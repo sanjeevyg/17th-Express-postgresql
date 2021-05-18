41dec093a448975f620d33afb8cb4b7e7f860dce
sanjeevyg <yogi.sjv@gmail.com>
2021-05-18 16:43:43 -0500

    body-parser, cors and express added

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors")
 const app = express();
 
-app.listen(9000, () => {
+const PORT = 9000 
+
+app.listen(PORT, () => {
     console.log("server is listening at 9000")
 })
+
+app.get('/',(request, response) => {
+    response.json({message: "Welcome to the nod-epxress"})
+})
+
+
 
